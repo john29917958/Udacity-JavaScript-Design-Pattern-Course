7462212ba28f6f64b88a1b32b7db46ce3adf0a69
John Wang <john29917958@gmail.com>
2015-05-18 14:36:07 +0800

    Requirements 2 - Two cats in total.

--- javascripts/app/main.js
+++ javascripts/app/main.js
@@ -1,10 +1,19 @@
-var catClicker = document.getElementsByClassName('cat-clicker');
+var Cat = require('./Cat.js'),
+	CatUI = require('./CatUI.js'),
+	cats = document.getElementsByClassName('cat-clicker');
 
-for (var i = 0; i < catClicker.length; i++) {
-	catClicker[i].firstElementChild.addEventListener('click', function() {
-		var counter = catClicker[0].lastElementChild.lastElementChild,
-			currentCount = Number(counter.innerText);
-		
-		counter.innerHTML = currentCount + 1;
-	}, false);
+function initCats(cats) {
+	var currentCat,
+		currentCatName,
+		currentCatPhotoPath;
+
+	for (var i = 0; i < cats.length; i++) {
+		currentCat = cats[i];
+		currentCatName = currentCat.firstElementChild.innerText;
+		currentCatPhotoPath = currentCat.children[1].firstElementChild.src;
+
+		new CatUI(currentCat, new Cat(currentCatName, currentCatPhotoPath));
+	}
 }
+
+initCats(cats);
